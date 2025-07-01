059b9f4d216c62465e80777d9a950ac90ca4e62f
RandonyV <ykygpro@mail.ru>
2025-07-01 11:51:31 +0300

    Fixed Fractals
    Now fractals are spawning normaly

--- Assets/Scripts/Fractals Scripts/CloneManagment.cs
+++ Assets/Scripts/Fractals Scripts/CloneManagment.cs
@@ -6,12 +6,18 @@ using UnityEditor.Callbacks;
 using NUnit.Framework;
 using Unity.VisualScripting.FullSerializer;
 using System.Collections;
+using UnityEngine.Animations;
+using UnityEngine.UIElements;
 public class CloneManagment : MonoBehaviour
 {
+    Animator Anim;
+    
 
     public IEnumerator DelayedDestroy(float delay)
     {
         yield return new WaitForSeconds(delay);
+        Anim.SetTrigger("ScaleOut");
+        yield return new WaitForSeconds(1.5f);
         Destroy(gameObject);
         FractalsSpawner.CloneQuantity -= 1;
     }
@@ -19,18 +25,18 @@ public class CloneManagment : MonoBehaviour
     // Position
     void RandomPosition()
     {
-        int PositionZ;
-        int PositionX = rnd.Next(-300, 300);
-        int PositionY = rnd.Next(-50, 400);
-        int RandZ = rnd.Next(0, 1);
+        float PositionZ;
+        float PositionX = GameObject.Find("Player").transform.position.x + rnd.Next(-300, 300);
+        float PositionY = GameObject.Find("Player").transform.position.y + rnd.Next(-50, 400);
+        float RandZ = rnd.Next(0, 1);
         if (Math.Sqrt(math.square(PositionX) + math.square(PositionY)) < 100)
         {
-            if (RandZ == 1) PositionZ = rnd.Next(200, 300);
-            else PositionZ = rnd.Next(-300, -200);
+            if (RandZ == 1) PositionZ = GameObject.Find("Player").transform.position.z + rnd.Next(200, 300);
+            else PositionZ = GameObject.Find("Player").transform.position.z + rnd.Next(-300, -200);
         }
         else
         {
-            PositionZ = rnd.Next(-300, 300);
+            PositionZ = GameObject.Find("Player").transform.position.z + rnd.Next(-300, 300);
         }
         transform.position = new UnityEngine.Vector3(PositionX, PositionY, PositionZ);
     }
@@ -41,8 +47,9 @@ public class CloneManagment : MonoBehaviour
     float SpeedZ;
     void Start()
     {
-        // GameObject Spawner = GameObject.Find("Fractals");
-        
+        // Animator
+        Anim = GetComponent<Animator>();
+        // Anim.
 
         // Position
         RandomPosition();
@@ -54,7 +61,7 @@ public class CloneManagment : MonoBehaviour
 
         // Destruction
         StartCoroutine(DelayedDestroy(
-            rnd.Next(5, 15)
+            rnd.Next(5, 13)
             ));
     }
 
